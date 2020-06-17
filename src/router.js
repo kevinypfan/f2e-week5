18394e0b53a5675a78986b02aa2c68d3025e3a8c
KevinFan <kevinypfan@gmail.com>
2020-06-18 01:57:25 +0800

    +: history mode and vue config js

--- src/router.js
+++ src/router.js
@@ -1,21 +1,22 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Reading from './views/Reading.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Reading from "./views/Reading.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/reading',
-      name: 'reading',
-      component: Reading
-    }
-  ]
+      path: "/reading",
+      name: "reading",
+      component: Reading,
+    },
+  ],
 });
